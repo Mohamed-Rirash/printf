bb296b04f94ac1dcab21da5a9b72292d02ab7589
Mohamed_rirash <mohamedrirash12045@gmail.com>
2023-07-25 10:33:49 +0300

    initialization of function that prints hexadecimal

---
+++ printf_hex.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_hex - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_hex(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+	return (counter);
+}
