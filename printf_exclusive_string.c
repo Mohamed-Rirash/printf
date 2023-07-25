559003299aee16fdfe1f4bfff75b38805d1a4ca1
Abdulshakourr <shakirteck4@gmail.com>
2023-07-25 04:37:48 -0700

    fixed eror

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -14,11 +14,11 @@ int printf_exclusive_string(va_list val)
 	s = va_arg(val, char *);
 	if (s == NULL)
 		s = "(null)";
-	for (i = 0; s[i] != '\1'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] < 32 || s[i] >= 127)
 		{
-			_putchar('\');
+			_putchar('\\');
 			_putchar('x');
 			len = len + 2;
 			cast = s[i];
@@ -32,6 +32,7 @@ int printf_exclusive_string(va_list val)
 		else
 		{
 			_putchar(s[i]);
+			len++;
 		}
 	}
 	return (len);
