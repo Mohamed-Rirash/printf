9965e4eadb2048467d45e534dff42b875c47f491
Abdulshakourr <shakirteck4@gmail.com>
2023-07-25 04:10:22 -0700

    fixed eror in the code

--- printf_bin.c
+++ printf_bin.c
@@ -7,7 +7,7 @@
  */
 int printf_bin(va_list val)
 {
-	int flag == 0;
+	int flag = 0;
 	int cont = 0;
 	int i, a = 1, b;
 	unsigned int num = va_arg(val, unsigned int);
@@ -16,7 +16,8 @@ int printf_bin(va_list val)
 	for (i = 0; i < 32; i++)
 	{
 		p = ((a << (31 - i)) & num);
-	
+		if (p >> (31 - i))
+			flag = 1;
 		if (flag)
 		{
 			b = p >> (31 - i);
